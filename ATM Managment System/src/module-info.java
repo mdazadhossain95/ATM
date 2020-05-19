3a9678aff0f5b99fa190d5cb0427ded0d4f16308
Md Azad Hossain Tutul <58014179+mdazadhossain95@users.noreply.github.com>
2020-05-18 18:53:44 -0700

    add new files
    created fxml file

---
+++ ATM Managment System/src/module-info.java
@@ -0,0 +1,11 @@
+module ATM.Managment.System {
+
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires java.desktop;
+
+    opens sample;
+
+
+}
--- ATM Managment System/src/sample/Controller.java
+++ ATM Managment System/src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Controller {
+
+    public void aVoid(ActionEvent actionEvent) throws SQLException{
+
+        DatabaseConnector databaseConnector = new DatabaseConnector();
+        Connection connection = databaseConnector.getConnection();
+    }
+
+
 }
